32cbda6b196a42309c449079f7363491254bc00e
Devin Mullin <devin.mullin90@gmail.com>
2021-11-23 14:49:43 -0600

    done

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,11 @@
-export function manageFriends(state, action) {
-  // your code here
+export function manageFriends(state = { friends: [] }, action) {
+  switch (action.type) {
+    case "friends/add":
+      return {...state, friends: [...state.friends, action.payload]}
+      case "friends/remove":
+        return {...state, friends: state.friends.filter(friends => friend.id !==action.payload)}
+        default:
+          return state
+  }
+
 }
--- src/managePresents.js
+++ src/managePresents.js
@@ -1,3 +1,11 @@
 export function managePresents(state, action) {
-  // your code here
+ let state = { presents: 0 }
+ let action = {type: "presents/increase"}
+ 
+  switch (action.type) {
+   case "presents/increase":
+    return {...state, numberOfPresents: state.numberOfPresents + 1 }
+    default: 
+      return state
+ }
 }
